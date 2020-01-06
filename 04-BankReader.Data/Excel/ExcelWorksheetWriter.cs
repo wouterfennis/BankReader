2eaf635eb475d8ae7613385c56fed6dd09313d51
Wouter Fennis <wouter_hilversum@hotmail.com>
2020-01-06 22:11:09 +0100

    Started with ExcelWorksheetWriter

---
+++ 04-BankReader.Data/Excel/ExcelWorksheetWriter.cs
@@ -0,0 +1,68 @@
+﻿using OfficeOpenXml;
+using System.Drawing;
+
+namespace BankReader.Data.Excel
+{
+    class ExcelWorksheetWriter : IWorksheetWriter
+    {
+        public ExcelWorksheet ExcelWorksheet { get; }
+        public Point CurrentPosition { get; private set; }
+
+        private ExcelRange CurrentCell
+        {
+            get
+            {
+                return ExcelWorksheet.Cells[CurrentPosition.X, CurrentPosition.Y];
+            }
+        }
+
+        public ExcelWorksheetWriter(ExcelWorksheet excelWorksheet, Point startPoint)
+        {
+            ExcelWorksheet = excelWorksheet;
+            CurrentPosition = startPoint;
+        }
+
+        public void SetCurrentPosition(Point newPosition)
+        {
+            CurrentPosition = newPosition;
+        }
+
+        public void MoveDown()
+        {
+            CurrentPosition = new Point(CurrentPosition.X, CurrentPosition.Y + 1);
+        }
+
+        public void MoveLeft()
+        {
+            CurrentPosition = new Point(CurrentPosition.X - 1, CurrentPosition.Y);
+        }
+
+        public void MoveRight()
+        {
+            CurrentPosition = new Point(CurrentPosition.X + 1, CurrentPosition.Y);
+        }
+
+        public void MoveUp()
+        {
+            CurrentPosition = new Point(CurrentPosition.X, CurrentPosition.Y - 1);
+        }
+
+        public void SetColor(Color newColor)
+        {
+            CurrentCell.Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+            CurrentCell.Style.Fill.BackgroundColor.SetColor(newColor);
+        }
+
+        public void SetData(decimal value)
+        {
+            CurrentCell.Style.Numberformat.Format = "€#,##0.00";
+            CurrentCell.Value = value;
+        }
+
+        public void SetData(string value)
+        {
+            CurrentCell.Value = value;
+        }
+
+    }
+}
---
+++ 04-BankReader.Data/Excel/IWorksheetWriter.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+namespace BankReader.Data.Excel
+{
+    public interface IWorksheetWriter
+    {
+        void SetCurrentPosition(Point newPosition);
+
+        void MoveRight();
+
+        void MoveLeft();
+
+        void MoveUp();
+
+        void MoveDown();
+
+        void SetColor(Color newColor);
+
+        void SetData(decimal value);
+
+        void SetData(string value);
+    }
+}
