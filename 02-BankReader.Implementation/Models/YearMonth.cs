7bef2ce4222647bf8fbd1eee726c7eaede4e9061
Wouter Fennis <WouterF@infosupport.com>
2020-06-04 20:56:44 +0200

    Value objects please

---
+++ 02-BankReader.Implementation/Models/YearMonth.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankReader.Implementation.Models
+{
+    internal sealed class YearMonth : ValueObject
+    {
+        private const int MinimalMonth = 1;
+        private const int MaximalMonth = 12;
+
+        public YearMonth(int year, int month)
+        {
+            Year = year;
+            Month = month;
+        }
+
+        /// <summary>
+        /// Gives year part.
+        /// </summary>
+        public Year Year { get; }
+
+        /// <summary>
+        /// Gives month part.
+        /// </summary>
+        public Month Month { get; }
+
+        /// <summary>
+        /// Creates a YearMonth from integers
+        /// </summary>
+        public static YearMonth FromInt32(int year, int month) => new YearMonth(year, month);
+
+        protected override IEnumerable<object> GetEqualityComponents() => new object[] { Year, Month };
+    }
+}
---
+++ 03-BankReader.Implementation.UnitTests/Models/YearMonthTests.cs
@@ -0,0 +1,56 @@
+﻿using AutoFixture;
+using BankReader.Implementation.Models;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BankReader.Implementation.UnitTests.Models
+{
+    [TestClass]
+    public class YearMonthTests
+    {
+        private Fixture _fixture;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _fixture = new Fixture();
+        }
+
+        [TestMethod]
+        public void Create_WithValidYearAndMonth_CreatesYearMonthType()
+        {
+            int validYear = 2100;
+            int validMonth = 12;
+
+            YearMonth yearMonth = new YearMonth(validYear, validMonth);
+
+            yearMonth.Year.Value.Should().Be(validYear);
+            yearMonth.Month.Value.Should().Be(validMonth);
+        }
+
+        [TestMethod]
+        public void Create_WithInValidYear_ThrowsException()
+        {
+            int invalidYear = 2101;
+            int month = 12;
+
+            Action action = () => { new YearMonth(invalidYear, month); };
+
+            action.Should()
+                .Throw<ArgumentException>();
+        }
+
+        [TestMethod]
+        public void Create_WithInValidMonth_ThrowsException()
+        {
+            int year = 2100;
+            int invalidMonth = 13;
+
+            Action action = () => { new YearMonth(year, invalidMonth); };
+
+            action.Should()
+                .Throw<ArgumentException>();
+        }
+    }
+}
